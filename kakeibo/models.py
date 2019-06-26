848f0d5f69a52f82a9fa35b9d33f20e35bf8db7d
uni51 <uni51@users.noreply.github.com>
2019-06-26 10:56:23 +0900

    modify comment

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -6,12 +6,12 @@ class Category(models.Model):
     class Meta:
         #テーブル名の指定
         db_table ="category"
-        verbose_name ="カテゴリ"
-        verbose_name_plural ="カテゴリ"      
+        verbose_name ="カテゴリ" # 表示名の単数形
+        verbose_name_plural ="カテゴリ" # 表示名の複数形      
      
     #カラム名の定義
     category_name = models.CharField(max_length=255, unique=True)
-    
+    # adminサイトで表示される文字列を定義する
     def __str__(self):
         return self.category_name    
 
@@ -28,6 +28,6 @@ class Kakeibo(models.Model):
     category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
     money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
     memo = models.CharField(verbose_name="メモ", max_length=500)
-
+    # adminサイトで表示される文字列を定義する
     def __str__(self):
         return self.memo
